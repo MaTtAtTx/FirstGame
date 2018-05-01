39079b68b34c7d07e0ca3f2ae0045417564d8385
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-05-01 09:19:53 -0600

    Added all the code to allow enemies to fly and need to make sure everything is before base and end.

--- FirstGame/Controller/SpaceGame.cs
+++ FirstGame/Controller/SpaceGame.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using FirstGame.Model;
 using FirstGame.View;
+using System.Collections.Generic;
 
 namespace FirstGame.Controller
 {
@@ -21,20 +22,29 @@ namespace FirstGame.Controller
 		private Texture2D mainBackground;
 		private ParallaxingBackground bgLayer1;
 		private ParallaxingBackground bgLayer2;
+		private Texture2D enemyTexture;
+		private List<Enemy> enemies;
+		private TimeSpan enemySpawnTime;
+		private TimeSpan previousSpawnTime;
+		private Random random;
 
 		public SpaceGame()
 		{
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
-			player = new Player();
-			bgLayer1 = new ParallaxingBackground();
-			bgLayer2 = new ParallaxingBackground();
 		}
 
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
 
+			player = new Player();
+			bgLayer1 = new ParallaxingBackground();
+			bgLayer2 = new ParallaxingBackground();
+			enemies = new List<Enemy>();
+			previousSpawnTime = TimeSpan.Zero;
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+			random = new Random();
 			base.Initialize();
 		}
 
@@ -60,6 +70,8 @@ namespace FirstGame.Controller
 			player.Initialize(playerAnimation, playerPosition);
 
 			playerMoveSpeed = 8.0f;
+
+			enemyTexture = Content.Load<Texture2D>("Animation/mineAnimation");
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -83,6 +95,8 @@ namespace FirstGame.Controller
 			// Update the parallaxing background
 			bgLayer1.Update();
 			bgLayer2.Update();
+
+			UpdateEnemies(gameTime);
 		}
 
 		private void UpdatePlayer(GameTime gameTime)
@@ -90,30 +104,30 @@ namespace FirstGame.Controller
 			player.Update(gameTime);
 
 			// Get Thumbstick Controls
-   	 		player.Position.X += currentGamePadState.ThumbSticks.Left.X* playerMoveSpeed;
-			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y* playerMoveSpeed;
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
 
-    		// Use the Keyboard / Dpad
-    		if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
-    		{
-      		  	player.Position.X -= playerMoveSpeed;
-   			}
-    		if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
-    		{
-        		player.Position.X += playerMoveSpeed;
-    		}
-    		if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
-    		{
-    			player.Position.Y -= playerMoveSpeed;
-    		}
-    		if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
-    		{		
-        		player.Position.Y += playerMoveSpeed;
-    		}
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
 
-    		// Make sure that the player does not go out of bounds
-    		player.Position.X = MathHelper.Clamp(player.Position.X, 0,GraphicsDevice.Viewport.Width - player.Width);
-    		player.Position.Y = MathHelper.Clamp(player.Position.Y, 0,GraphicsDevice.Viewport.Height - player.Height);
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
 		}
 
 		protected override void Draw(GameTime gameTime)
@@ -126,8 +140,57 @@ namespace FirstGame.Controller
 			bgLayer1.Draw(spriteBatch);
 			bgLayer2.Draw(spriteBatch);
 			player.Draw(spriteBatch);
+			for (int i = 0; i<enemies.Count; i++)
+			{
+				enemies[i].Draw(spriteBatch);
+			}
 			spriteBatch.End();
 			base.Draw(gameTime);
+
+		}
+
+		private void AddEnemy()
+		{
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30, Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position);
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--)
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					enemies.RemoveAt(i);
+				}
+			}
 		}
 	}
 }
